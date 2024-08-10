35600e474371eddf344106ee9620d5436550e0d7
Autumn <austinnbrown95@gmail.com>
2024-08-10 15:24:05 -0500

    GetWeather added

--- src/Page1.js
+++ src/Page1.js
@@ -2,12 +2,14 @@ import React from 'react';
 import '@aws-amplify/ui-react/styles.css';
 import Container from './Container';
 import Nav from './Nav';
+import GetWeather from './GetWeather';
 
 const Page1 = () => {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello</h1>
+        <GetWeather></GetWeather>
       </header>
     </div>
   );
